bb46c83158b1de8817805efff0ea016382e7dd92
Patryk Janik <pumanitro@gmail.com>
2020-04-20 11:24:54 +0200

    Added posibility of changing breakpoints for RWD utils.

--- src/RWDProvider.tsx
+++ src/RWDProvider.tsx
@@ -13,7 +13,9 @@ export interface RWDDataType {
 
 export type RWDContextData = RWDDataType | {};
 
-const getRWDData = (): RWDContextData => {
+export type BreakpointsType = { [key: string]: number; }
+
+const getRWDData = (breakpoints: BreakpointsType): RWDContextData => {
   // For server side rendering:
   const isClient: boolean = typeof window === 'object';
 
@@ -30,20 +32,24 @@ const getRWDData = (): RWDContextData => {
   } as any) as RWDDataType;
 
   BreakpointsKeys.forEach(key => {
-    RWDData.less[key] = window.innerWidth <= BREAKPOINTS[key];
-    RWDData.more[key] = window.innerWidth > BREAKPOINTS[key];
+    RWDData.less[key] = window.innerWidth <= breakpoints[key];
+    RWDData.more[key] = window.innerWidth > breakpoints[key];
   });
 
   return RWDData;
 };
 
-export const RWDProvider: FunctionComponent = ({ children }) => {
-  const [RWDData, setRWDData] = useState(getRWDData());
+export type RWDProviderProps = {
+  breakpoints: BreakpointsType,
+};
+
+export const RWDProvider: React.FunctionComponent<RWDProviderProps> = ({ children, breakpoints = BREAKPOINTS }) => {
+  const [RWDData, setRWDData] = useState(getRWDData(breakpoints));
 
   useEffect(() => {
-    window.addEventListener('resize', () => setRWDData(getRWDData()), false);
+    window.addEventListener('resize', () => setRWDData(getRWDData(breakpoints)), false);
 
-    return window.removeEventListener('resize', () => setRWDData(getRWDData()), false);
+    return window.removeEventListener('resize', () => setRWDData(getRWDData(breakpoints)), false);
   }, []);
 
   return <RWDContext.Provider value={RWDData}>{children}</RWDContext.Provider>;
